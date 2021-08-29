418c7614c1b94bc20d8b54be0d8a3f490bfd924b
pavithrapaudel <pavithrapaudel98@gmail.com>
2021-08-29 13:08:33 +0530

    pavithra14

--- dir1/src/app/directive/directive.component.ts
+++ dir1/src/app/directive/directive.component.ts
@@ -9,6 +9,14 @@ export class DirectiveComponent implements OnInit {
 
   title = 'Directive';
   display = false;
+  username ="gowthu";
+   users = [
+     {name:"pavi", id:"01", age:"20"},
+     {name:"saro", id:"02", age:"21"},
+     {name:"renu", id:"03", age:"22"},
+     {name:"chandra", id:"04", age:"23"},
+     {name:"bakku",id:"05", age:"24"}
+   ];
   constructor() { }
 
   ngOnInit(): void {
