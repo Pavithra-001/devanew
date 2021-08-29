7fb24d1f3df1715470a95d4b425e195f822c6a90
pavithrapaudel <pavithrapaudel98@gmail.com>
2021-08-29 13:03:16 +0530

    pavithra12

--- dir1/src/app/directive/directive.component.ts
+++ dir1/src/app/directive/directive.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DirectiveComponent implements OnInit {
 
   title = 'Directive';
+  display = false;
   constructor() { }
 
   ngOnInit(): void {
