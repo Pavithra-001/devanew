743f5a80065b85cdbfaef31c3f1ff3999bfc6321
pavithrapaudel <pavithrapaudel98@gmail.com>
2021-08-29 20:32:36 +0530

    pavithra24

--- dir1/src/app/app-routing.module.ts
+++ dir1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'department', component: DepartmentComponent},
+  {path: 'employeelist', component: EmployeelistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
